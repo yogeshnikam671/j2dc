560814f84ac5b3879ce83bee0a04e3009128389c
Yogesh Nikam <yogeshnikam671@gmail.com>
2023-01-03 17:15:52 +0530

    Add functionality to figure out nested objects and create data classes for those

--- index.js
+++ index.js
@@ -7,44 +7,80 @@ const obj = {
   "b": "hello",
   "c": true,
   "d": ["something", "else"],
-  "e": { }
+  "e": { },
+  "f": {
+    "f1": "hello",
+   },
+  "g": {
+    "g1": { "random": 10 },
+    "g2": {
+        "g3": {
+            "g4": [],
+            "g5": {
+                "g6": 10
+             }
+         }
+     }
+   }
 };
 
-const object1 = "non"
+const obj1 = "non"
 
-const createDataClassFrom = (object) => {
-  if(!isObject(object)) {
-    console.log("Invalid JSON object");
-    cp.copy("Invalid JSON object");
-    return;
-  }
+const createDataClassFromHelper = (object) => {
   let res = "data class J2DC(\n";
-  
+  const subTypeMap = {};
+
+  res = createDataClassFrom(object, res, subTypeMap);
+
+  while(Object.keys(subTypeMap).length > 0) {
+    Object.keys(subTypeMap).forEach(key => {
+      res += `\ndata class ${key}(\n`;
+      res = createDataClassFrom(subTypeMap[key], res, subTypeMap);
+      delete subTypeMap[key];
+    });
+  }
+
+  console.log(res);
+  cp.copy(res);
+};
+
+const createDataClassFrom = (object, res, subTypeMap) => {
+  if(!isObject(object)) {
+    return "Invalid JSON object";
+  }
+
   Object.keys(object).forEach(key => {
-    const type = kotlinTypeOf(object[key]);
+    const type = kotlinTypeOf(object[key], key);
     res = res + `\tval ${key}: ${type}?,\n`;
+    addSubTypeDetails(subTypeMap, object, key, type);
   });
 
   res = res + ")\n";
-  console.log(res);
-  cp.copy(res);
+  return res;
 }
 
-const kotlinTypeOf = (value) => {
+const addSubTypeDetails = (subTypeMap, object, key, type) => {
+  if(type.toLowerCase() === key.toLowerCase()) {
+    subTypeMap[type] = object[key];
+  }
+}
+
+const kotlinTypeOf = (value, key) => {
   switch(typeof value) {
     case "number": return "Int";
     case "string": return "String";
     case "boolean": return "Boolean";
     case "object": {
-      if(isObject(value)) return kotlinTypeOfObject(value);
+      if(isObject(value)) return kotlinTypeOfObject(value, key);
       else return kotlinTypeOfArray(value);
     }; 
     default: return "Any";
   }
 } 
 
-const kotlinTypeOfObject = (object) => {
-  return "Any"
+const kotlinTypeOfObject = (object, key) => {
+  if(Object.keys(object).length === 0) return "Any";
+  return key; // TODO - make this a normal first character capital and other characters small word
 }
 
 const kotlinTypeOfArray = (array) => {
@@ -71,13 +107,13 @@ const execute = () => {
   if (process.argv.length === 2) {
     try {
       const copiedContent = JSON.parse(cp.paste());
-      createDataClassFrom(copiedContent);
+      createDataClassFromHelper(copiedContent);
     } catch (e) {
       console.log("Invalid JSON data");
     }
   } else if(process.argv[2]) {
     switch(process.argv[2]) {
-      case '-i' : createDataClassFrom(obj); break;
+      case '-i' : createDataClassFromHelper(obj); break;
       default: console.log("Invalid flag"); 
     }
   }
